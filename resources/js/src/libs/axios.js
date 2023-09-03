08e5d68809c8cee3fa9cd0003634ded1122fd133
arnonr <arnonr@kmutnb.ac.th>
2023-09-04 01:53:54 +0700

    axios

--- resources/js/src/libs/axios.js
+++ resources/js/src/libs/axios.js
@@ -3,11 +3,21 @@ import Vue from 'vue'
 // axios
 import axios from 'axios'
 
+let urlNew = '';
+if(location.port  == '8000'){
+    urlNew = 'http://localhost:8000/api/';
+}else{
+    if(location.hostname == 'localhost'){
+        urlNew = 'http://localhost/research/api/';
+    }else{
+        urlNew = 'http://sci.kmutnb.ac.th/research/api/';
+    }
+}
 
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: location.port == '8000' ? 'http://localhost:8000/api/' : 'http://localhost/research/api/',
+  baseURL: urlNew,
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 })
